c049e525c3b68f36dac0d660ec6dcf84ac97a7b0
Zhenia <zhenia1985@gmail.com>
2023-02-20 20:33:46 +0700

    feat: base entity

--- src/offers/entities/offer.entity.ts
+++ src/offers/entities/offer.entity.ts
@@ -1,1 +1,24 @@
-export class Offer { }
+import { Base } from "src/utils/base.entity";
+import { Column } from "typeorm";
+
+export class Offer extends Base {
+   @Column()
+   //user содержит id желающего скинуться;
+
+   @Column()
+   //item содержит ссылку на товар;
+
+   @Column()
+   //amount — сумма заявки, округляется до двух знаков после запятой;
+
+   @Column();
+   //hidden — флаг, который определяет показывать ли информацию о скидывающемся в списке. По умолчанию равен false.
+
+
+   //связи
+   //@OneToOne()
+
+   //@OneToMany()
+
+   //@ManyToMany()
+}
--- src/wishlists/entities/wishlist.entity.ts
+++ src/wishlists/entities/wishlist.entity.ts
@@ -1,1 +1,22 @@
-export class Wishlist { }
+import { Base } from "src/utils/base.entity";
+
+export class Wishlist extends Base {
+   @Column();
+   //name — название списка. Не может быть длиннее 250 символов и короче одного;
+
+   @Column();
+   description — описание подборки, строка до 1500 символов;
+
+   @Column();
+   //image — обложка для подборки;
+
+   @Column();
+   //items содержит набор ссылок на подарки.
+
+   //связи
+   //@OneToOne()
+
+   //@OneToMany()
+
+   //@ManyToMany()
+}
